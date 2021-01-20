f546a49c3e603a3d33723fa5b5d78f28fc0711ae
Nikola <nstipic@etfos.hr>
2021-01-20 18:07:54 +0100

    Inicijalni commit

---
+++ Zadatak1/Prvi zadatak/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prvi_zadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
